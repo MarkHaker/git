9c84ed79326daad708e66b8f7c7861eba6901158
Mark Mayer <markmayer00@yandex.ru>
2025-03-15 21:11:18 +0300

    отдыхай в бане и - ссылки

--- bot.py
+++ bot.py
@@ -22,4 +22,20 @@ def ban_user(message):
     else:
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
 
+@bot.message_handler(func=lambda message: True) # Обработчик всех сообщений
+def check_links(message):
+    if "https://" in message.text or "http://" in message.text:
+        # Запрещаем ссылки
+        chat_id = message.chat.id
+        user_id = message.from_user.id
+        try:
+            username = message.from_user.username
+        except:
+            username = "Пользователь без имени пользователя"
+        with open("banned_users.txt", "a") as f:
+            f.write(f"User ID: {user_id}, Username: {username}, Message: {message.text}\n")
+
+        bot.ban_chat_member(chat_id, user_id)
+        bot.reply_to(message, f"@{username} забанен за публикацию ссылки.")
+
 bot.infinity_polling(none_stop=True)
--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-token = "" # токен для бота
+token = "7845442378:AAGXcbyYMilk_Z6KWWt0IeCTC2B3T0c0Z1w" # токен для бота
